13ab4c357a9d4d6778a1120460bf5c3f9e32fb28
ZhuRui Xiang <xiangzhurui@gmail.com>
2018-08-14 20:13:34 +0800

    add drools module

---
+++ demo-drools/src/main/java/com/example/demo/drools/DemoDroolsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDroolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDroolsApplication.class, args);
+    }
+}
---
+++ demo-spring-boot-cache/src/main/java/com/example/demo/spring/cache/DemoSpringCacheApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.spring.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringCacheApplication.class, args);
+    }
+}
