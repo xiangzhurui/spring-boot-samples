9f1dde5b8eab0926bae3d5ce1d904e4e3b15849f
ZhuRui Xiang <xiangzhurui@gmail.com>
2018-08-22 16:47:56 +0800

    add a spark module struct

---
+++ demo-spark-streaming/src/main/java/com/example/demo/spark/streaming/DemoSparkStreamingApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.spark.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSparkStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSparkStreamingApplication.class, args);
+    }
+}
