fcefa8ad24522ea3ec1e300aa8a4f7bd6cecf6ab
ZhuRui Xiang <xiangzhurui@gmail.com>
2018-09-03 15:55:38 +0800

    add demo-spring-boot-web-jsp

---
+++ demo-spring-boot-web-jsp/src/main/java/com/example/jsp/JspWebApplication.java
@@ -0,0 +1,20 @@
+package com.example.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JspWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JspWebApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(JspWebApplication.class, args);
+	}
+
+}
