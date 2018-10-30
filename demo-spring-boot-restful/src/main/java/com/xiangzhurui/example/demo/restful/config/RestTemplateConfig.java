d863618eca5a7549984fa3a11377ae14ca4c8176
zhurui.xiang <xiangzhurui@gmail.com>
2018-10-30 13:11:32 +0800

    RestTemplate 配置

--- demo-spring-boot-restful/src/main/java/com/xiangzhurui/example/demo/restful/config/RestTemplateConfig.java
+++ demo-spring-boot-restful/src/main/java/com/xiangzhurui/example/demo/restful/config/RestTemplateConfig.java
@@ -1,19 +1,169 @@
 package com.xiangzhurui.example.demo.restful.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.AuthSchemes;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author xiangzhurui
  * @version 2018/10/29 20:14
  */
+@Slf4j
 @Configuration
 public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
+        return restTemplate;
+    }
+
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        ClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(httpClient());
+        return clientHttpRequestFactory;
+    }
+
+    /**
+     * 使用连接池的 HttpClient 实例
+     *
+     * @return
+     */
+    @Bean
+    public HttpClient httpClient() {
+        CloseableHttpClient closeableHttpClient = HttpClients.custom()
+                .setConnectionManager(getPoolingHttpClientConnectionManager())
+                .setConnectionManagerShared(true)
+                .setDefaultRequestConfig(getRequestConfig())
+                // 设置重试次数
+                .setRetryHandler(getRetryHandler())
+                .build();
+        return closeableHttpClient;
+    }
+
+    /**
+     * HTTP 请求重试次数
+     *
+     * @return
+     */
+    private DefaultHttpRequestRetryHandler getRetryHandler() {
+        return new DefaultHttpRequestRetryHandler(3, false);
+    }
+
+
+    /**
+     * HTTP 连接池配置
+     *
+     * @return
+     */
+    private PoolingHttpClientConnectionManager getPoolingHttpClientConnectionManager() {
+
+        SSLConnectionSocketFactory sslConnectionSocketFactory = getSslConnectionSocketFactory();
+
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.INSTANCE)
+                .register("https", sslConnectionSocketFactory)
+                .build();
+
+        PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+
+        // 连接池最大并发连接数
+        poolingHttpClientConnectionManager.setMaxTotal(100);
+
+        // 单路由最大并发数
+        poolingHttpClientConnectionManager.setDefaultMaxPerRoute(20);
+
+        return poolingHttpClientConnectionManager;
+    }
+
+    private SSLConnectionSocketFactory getSslConnectionSocketFactory() {
+        TrustManager trustManager = new X509TrustManager() {
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+        };
+
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{trustManager}, null);
+
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            log.error("init SSLContext fail! ", e);
+        }
+        return new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+    }
+
+    public SSLConnectionSocketFactory getSslConnectionSocketFactory1() {
+        SSLContextBuilder builder = new SSLContextBuilder();
+        try {
+            builder.loadTrustMaterial(null, new TrustAllStrategy());
+
+            SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(builder.build());
+            return sslConnectionSocketFactory;
+        } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * HTTP 请求配置
+     *
+     * @return
+     */
+    private RequestConfig getRequestConfig() {
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(200)
+                .setSocketTimeout(500)
+                .setConnectionRequestTimeout(200)
+                .setCookieSpec(CookieSpecs.STANDARD_STRICT)
+                .setExpectContinueEnabled(true)
+                .setTargetPreferredAuthSchemes(Arrays.asList(AuthSchemes.NTLM, AuthSchemes.DIGEST))
+                .setProxyPreferredAuthSchemes(Arrays.asList(AuthSchemes.BASIC))
+                .build();
+        return requestConfig;
+    }
 
 
 }
--- demo-spring-boot-restful/src/test/java/com/xiangzhurui/example/demo/restful/config/QueryParamsUrlTemplateHandlerTest.java
+++ demo-spring-boot-restful/src/test/java/com/xiangzhurui/example/demo/restful/config/QueryParamsUrlTemplateHandlerTest.java
@@ -1,16 +1,87 @@
 package com.xiangzhurui.example.demo.restful.config;
 
-import org.junit.Test;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
 
-import static org.junit.Assert.*;
+import com.google.common.collect.Maps;
+import com.xiangzhurui.example.demo.restful.RestfulDemoApplicationTests;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriTemplateHandler;
 
 /**
  * @author xiangzhurui
  * @version 2018/10/29 20:16
  */
-public class QueryParamsUrlTemplateHandlerTest {
+@Slf4j
+public class QueryParamsUrlTemplateHandlerTest extends RestfulDemoApplicationTests {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Test
     public void expand() {
+
+        QueryParamsUrlTemplateHandler queryParamsUrlTemplateHandler = new QueryParamsUrlTemplateHandler();
+        Map<String, String> uriVariables = Maps.newHashMap();
+
+        uriVariables.put("q", "测试用例");
+
+        String uriTemplate = "https://cn.bing.com/search";
+        URI uri = queryParamsUrlTemplateHandler.expand(uriTemplate, uriVariables);
+        System.out.println(uri.toString());
+
+        System.out.println(new DefaultUriTemplateHandler().expand("https://cn.bing.com/search/{q}", uriVariables).toASCIIString());
+
+
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(1000);
+        requestFactory.setReadTimeout(1000);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        restTemplate.setUriTemplateHandler(queryParamsUrlTemplateHandler);
+        String map = restTemplate.getForObject(uriTemplate, String.class, uriVariables);
+        System.out.println(map);
+    }
+
+    @Test
+    public void test() throws IOException {
+        RestTemplateConfig restTemplateConfig = new RestTemplateConfig();
+        HttpClient httpClient = restTemplateConfig.httpClient();
+
+        HttpResponse httpResponse = httpClient.execute(new HttpGet("https://cn.bing.com"));
+        HttpEntity httpEntity = httpResponse.getEntity();
+        String s = EntityUtils.toString(httpEntity);
+
+        System.out.println(s);
+
+    }
+
+    @Test
+    public void testGet() {
+        QueryParamsUrlTemplateHandler queryParamsUrlTemplateHandler = new QueryParamsUrlTemplateHandler();
+        Map<String, String> queryVariables = Maps.newHashMap();
+
+        queryVariables.put("q", "测试用例");
+
+        String uriTemplate = "https://cn.bing.com/search";
+        URI uri = queryParamsUrlTemplateHandler.expand(uriTemplate, queryVariables);
+
+        restTemplate.setUriTemplateHandler(queryParamsUrlTemplateHandler);
+        ResponseEntity<String> bingStr = restTemplate.getForEntity(uriTemplate, String.class, queryVariables);
+
+        log.info("访问必应搜索首页结果：{},{}", bingStr.getStatusCode().toString(),bingStr.getStatusCode().getReasonPhrase());
+        String s = Jsoup.parse(bingStr.getBody()).html();
+        log.info("格式化后的bing首页：\n {}", s);
     }
 }
